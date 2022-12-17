9301494c080275170dd3c601664963a07a84bfb9
DELL <DELL@SEMIH>
2022-12-17 15:07:27 +0300

    .

--- Infrastructure/ETicaretAPI.Persistence/Migrations/20221217120226_mig_1.cs
+++ Infrastructure/ETicaretAPI.Persistence/Migrations/20221217120226_mig_1.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ETicaretAPI.Persistence.Migrations
 {
     /// <inheritdoc />
-    public partial class mig1 : Migration
+    public partial class mig1 : Migration 
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
